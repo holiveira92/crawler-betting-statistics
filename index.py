9e45a05c344f7f019138ce445fe065aac49a5a0a
Hiago Oliveira <hiagojardimdeoliveira@hotmail.com>
2023-06-03 16:48:32 -0300

    extraindo mais dados do ht

--- index.py
+++ index.py
@@ -9,16 +9,20 @@ def getDataByUrlTeam(html_text):
     # Definindo área do DOM que contém as informações desejadas
     tables = soup.find_all("table", {"cellspacing":"0", "cellpadding":"1", "bgcolor":"#f8f8f8", "width":"99%"})
     
-    first_goal_array = getFirstGoalData(tables)
+    first_goal = getFirstGoalData(tables)
     half_time_leading = getHalfTimeLeadingData(tables)
+    opponent_scored_first = getOpponentScoredFirstData(tables)
+    opponent_leading_ht = getOpponentLeadingHTData(tables)
     
     return {
-        'first_goal_data' : first_goal_array,
-        'half_time_data' : half_time_leading
+        'first_goal' : first_goal,
+        'half_time_leading' : half_time_leading,
+        'opponent_scored_first' : opponent_scored_first,
+        'opponent_leading_half_time' : opponent_leading_ht,
     }
 
 def getFirstGoalData(tables):
-    first_goal_array = [] # [0] => Total , [1] => Home, [2] => Away
+    first_goal = [] # [0] => Total , [1] => Home, [2] => Away
     # itera nos itens encontrados na página
     for table in tables:
         row = ''
@@ -26,10 +30,10 @@ def getFirstGoalData(tables):
         for tr_row in tr_rows:
             if(tr_row.text.find("Team scored first") > -1):
                 percentage_position_array = 2
-                first_goal_array.append(
+                first_goal.append(
                     tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
                 )
-    return first_goal_array
+    return first_goal
 
 def getHalfTimeLeadingData(tables):
     half_time_leading = [] # [0] => Total , [1] => Home, [2] => Away
@@ -45,6 +49,34 @@ def getHalfTimeLeadingData(tables):
                 )
     return half_time_leading
 
+def getOpponentScoredFirstData(tables):
+    opponent_scored_first = [] # [0] => Total , [1] => Home, [2] => Away
+    # itera nos itens encontrados na página          
+    for table in tables:
+        row = ''
+        tr_rows = table.findAll('tr')
+        for tr_row in tr_rows:
+            if(tr_row.text.find("Opponent scored first") > -1):
+                percentage_position_array = 2
+                opponent_scored_first.append(
+                    tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
+                )
+    return opponent_scored_first
+
+def getOpponentLeadingHTData(tables):
+    opponent_leading_ht = [] # [0] => Total , [1] => Home, [2] => Away
+    # itera nos itens encontrados na página          
+    for table in tables:
+        row = ''
+        tr_rows = table.findAll('tr')
+        for tr_row in tr_rows:
+            if(tr_row.text.find("Opponent was leading at half-time") > -1):
+                percentage_position_array = 2
+                opponent_leading_ht.append(
+                    tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
+                )
+    return opponent_leading_ht
+
 
 def loadTeamsList():
     # JSON file
@@ -58,24 +90,24 @@ def loadTeamsList():
     
     return data
 
-def writeJsonFile(team_name, output_data):
-    json_final_data = []
-    json_final_data.append(json.dumps({
-        team_name : output_data
-    }))
-    
+def writeJsonFile(json_final_data):
     json_final_data = json.dumps(json_final_data)
-    with open(team_name + '.json', 'w') as outfile:
+    with open('output.json', 'w') as outfile:
         outfile.write(json_final_data)
     
 def executeCrawler(teams_list):
+    json_final_data = []
     for team_name in teams_list:
         json_data = requests.get(teams_list[team_name]).text
         output_data = getDataByUrlTeam(json_data)
         print(team_name)
         print(output_data)
-        writeJsonFile(team_name, output_data)
-
+        
+        json_final_data.append({
+            team_name : output_data
+        })
+    
+    writeJsonFile(json_final_data)
 
 # Início da execução do script
 teams_list = loadTeamsList()
