544edb05b645dd105560f1bb2233e2ab9f51e3b4
Hiago Oliveira <hiagojardimdeoliveira@hotmail.com>
2023-06-03 17:05:07 -0300

    refatorando funcoes

--- index.py
+++ index.py
@@ -9,10 +9,10 @@ def getDataByUrlTeam(html_text):
     # Definindo área do DOM que contém as informações desejadas
     tables = soup.find_all("table", {"cellspacing":"0", "cellpadding":"1", "bgcolor":"#f8f8f8", "width":"99%"})
     
-    first_goal = getFirstGoalData(tables)
-    half_time_leading = getHalfTimeLeadingData(tables)
-    opponent_scored_first = getOpponentScoredFirstData(tables)
-    opponent_leading_ht = getOpponentLeadingHTData(tables)
+    first_goal = getDataByKey(tables, "Team scored first")
+    half_time_leading = getDataByKey(tables, "Team was leading at half-time")
+    opponent_scored_first = getDataByKey(tables, "Opponent scored first")
+    opponent_leading_ht = getDataByKey(tables, "Opponent was leading at half-time")
     
     return {
         'first_goal' : first_goal,
@@ -21,62 +21,19 @@ def getDataByUrlTeam(html_text):
         'opponent_leading_half_time' : opponent_leading_ht,
     }
 
-def getFirstGoalData(tables):
-    first_goal = [] # [0] => Total , [1] => Home, [2] => Away
+def getDataByKey(tables, key):
+    response = [] # [0] => Total , [1] => Home, [2] => Away
     # itera nos itens encontrados na página
     for table in tables:
         row = ''
         tr_rows = table.findAll('tr')
         for tr_row in tr_rows:
-            if(tr_row.text.find("Team scored first") > -1):
+            if(tr_row.text.find(key) > -1):
                 percentage_position_array = 2
-                first_goal.append(
+                response.append(
                     tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
                 )
-    return first_goal
-
-def getHalfTimeLeadingData(tables):
-    half_time_leading = [] # [0] => Total , [1] => Home, [2] => Away
-    # itera nos itens encontrados na página          
-    for table in tables:
-        row = ''
-        tr_rows = table.findAll('tr')
-        for tr_row in tr_rows:
-            if(tr_row.text.find("Team was leading at half-time") > -1):
-                percentage_position_array = 2
-                half_time_leading.append(
-                    tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
-                )
-    return half_time_leading
-
-def getOpponentScoredFirstData(tables):
-    opponent_scored_first = [] # [0] => Total , [1] => Home, [2] => Away
-    # itera nos itens encontrados na página          
-    for table in tables:
-        row = ''
-        tr_rows = table.findAll('tr')
-        for tr_row in tr_rows:
-            if(tr_row.text.find("Opponent scored first") > -1):
-                percentage_position_array = 2
-                opponent_scored_first.append(
-                    tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
-                )
-    return opponent_scored_first
-
-def getOpponentLeadingHTData(tables):
-    opponent_leading_ht = [] # [0] => Total , [1] => Home, [2] => Away
-    # itera nos itens encontrados na página          
-    for table in tables:
-        row = ''
-        tr_rows = table.findAll('tr')
-        for tr_row in tr_rows:
-            if(tr_row.text.find("Opponent was leading at half-time") > -1):
-                percentage_position_array = 2
-                opponent_leading_ht.append(
-                    tr_row.findAll("td")[percentage_position_array].text.replace("%", "")
-                )
-    return opponent_leading_ht
-
+    return response
 
 def loadTeamsList():
     # JSON file
@@ -97,15 +54,21 @@ def writeJsonFile(json_final_data):
     
 def executeCrawler(teams_list):
     json_final_data = []
+    list_qty = len(teams_list)
+    count = 1
     for team_name in teams_list:
         json_data = requests.get(teams_list[team_name]).text
         output_data = getDataByUrlTeam(json_data)
-        print(team_name)
-        print(output_data)
+        print("------------------------------------------------------------------------------------------------------");
+        print( str(count) + "/" + str(list_qty))
+        print( team_name + " :" )
+        print( output_data )
+        print("------------------------------------------------------------------------------------------------------");
         
         json_final_data.append({
             team_name : output_data
         })
+        count = count + 1
     
     writeJsonFile(json_final_data)
 
